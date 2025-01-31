1735b9a37cf22e0070bb63b61a8482b737477073
John Doe <3vilbonobo@gmail.com>
2025-01-31 16:52:23 +0200

    create users-popup component

---
+++ src/app/components/user-popup/user-popup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-popup',
+  imports: [],
+  templateUrl: './user-popup.component.html',
+  styleUrl: './user-popup.component.css'
+})
+export class UserPopupComponent {
+
+}
