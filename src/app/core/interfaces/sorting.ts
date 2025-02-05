719736cbae48155f0ca2de00c4b04501eac2faf4
John Doe <3vilbonobo@gmail.com>
2025-02-05 16:19:27 +0200

    add basic sorting functionality

---
+++ src/app/core/interfaces/sorting.ts
@@ -0,0 +1,10 @@
+export interface Sorting {
+  id: 'asc' | 'desc' | 'none';
+  username: 'asc' | 'desc' | 'none';
+  password: 'asc' | 'desc' | 'none';
+  name: 'asc' | 'desc' | 'none';
+  surname: 'asc' | 'desc' | 'none';
+  email: 'asc' | 'desc' | 'none';
+  registrationDate: 'asc' | 'desc' | 'none';
+  isActive: 'asc' | 'desc' | 'none';
+}
--- src/app/features/users/users.component.ts
+++ src/app/features/users/users.component.ts
@@ -1,12 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, signal, ViewChild } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserModalComponent } from '../../shared/components/user-modal/user-modal.component';
 import { UserInterface } from '../../core/interfaces/user.interface';
+import { sortBy } from 'lodash';
+import { Sorting } from '../../core/interfaces/sorting';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faSortUp,
+  faSort,
+  faSortDown,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-users',
-  imports: [CommonModule, UserModalComponent],
+  imports: [CommonModule, UserModalComponent, FontAwesomeModule],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css'],
 })
@@ -15,25 +24,60 @@ export class UsersComponent {
 
   users = signal<UserInterface[]>([]); // Stores users
   selectedUser = signal<UserInterface | null>(null); // Tracks selected user
+  sortedUsers = signal<UserInterface[]>([]); // Stores sorted users
 
-  // Reference the modal component
-  @ViewChild(UserModalComponent) userModal!: UserModalComponent;
+  sortOrder: Sorting = {
+    id: 'none',
+    username: 'none',
+    password: 'none',
+    name: 'none',
+    surname: 'none',
+    email: 'none',
+    registrationDate: 'none',
+    isActive: 'none',
+  };
 
   constructor() {
     this.http.get<UserInterface[]>('http://localhost:3000/users').subscribe({
-      next: (data) => this.users.set(data),
+      next: (data) => {
+        this.users.set(data); // Store the original users
+        this.sortedUsers.set(data); // Store the sorted users
+      },
       error: (err) => console.error('Error fetching users:', err),
     });
   }
 
-  openModal(user: UserInterface) {
-    this.selectedUser.set(user);
-    if (this.userModal) {
-      this.userModal.setUser(user);
+  // Sort user table
+  sortData(sortKey: keyof Sorting): void {
+    // Toggle sorting order
+    const currentOrder = this.sortOrder[sortKey];
+    const newOrder = currentOrder === 'asc' ? 'desc' : 'asc';
+    this.sortOrder[sortKey] = newOrder;
+
+    const sorted = sortBy(this.users(), [sortKey]);
+
+    if (newOrder === 'desc') {
+      this.sortedUsers.set(sorted.reverse());
+    } else {
+      this.sortedUsers.set(sorted);
     }
   }
 
+  sortSign(sortKey: keyof Sorting): IconDefinition {
+    if (this.sortOrder[sortKey] === 'asc') {
+      return faSortUp; // Up arrow
+    } else {
+      return faSortDown; // Down arrow
+    }
+  }
+
+  openModal(user: UserInterface) {
+    // Set the selected user
+    this.selectedUser.set(user);
+  }
+
   closeModal() {
+    // Close modal and clear selected user
     this.selectedUser.set(null);
   }
 }
